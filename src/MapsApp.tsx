e49d41c8b2532aed1ab0b35d0f470bbeeedfcb03
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-10-02 13:58:53 -0300

    Show the user's geolocation on the screen

--- src/MapsApp.tsx
+++ src/MapsApp.tsx
@@ -1,9 +1,11 @@
 import { PlacesProvider } from "./context"
+import { HomeScreen } from "./screens"
+import './styles.css'
 
 export const MapsApp = () => {
   return (
     <PlacesProvider>
-        <h1>Hola mundo denuevo</h1>
+       <HomeScreen/>
     </PlacesProvider>
   )
 }
