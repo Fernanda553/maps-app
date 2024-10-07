06229a465e8d730259de692bc7cf6aec47aceb80
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-10-07 12:29:32 -0300

    Show Mapbox map

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MapsApp } from './MapsApp';
 
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZmVybmFuZGFudW5lemUiLCJhIjoiY20xejRsaWc3MDMyNjJpcHp2cmtxMnhmOSJ9.3b8KzYZVozkq0gaJPjKJJw';
+
 if(!navigator.geolocation){
   alert('Tu navegador no tiene la opción de Geolocation')
   throw new Error('Tu navegador no tiene la opción de Geolocation')
